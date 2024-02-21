348e75bb2458e75c8d9c1cb5a55c62b8477ac2e4
Emrah <emrahketzo@gmail.com>
2024-02-21 11:59:47 +0100

    plus de style

--- src/components/Panier/Panier.jsx
+++ src/components/Panier/Panier.jsx
@@ -5,7 +5,7 @@ export default function Panier(props) {
         <div className='monPanier'>
             {props.quantity > 0 && <div className='panier' id = {props.id}>
                 <img src={props.img} alt="" />
-                <p>{props.nom} {props.quantity}</p>
+                <p className='panierP'>{props.nom} {props.quantity}</p>
                 <div className='btnRendre' onClick={()=> props.retirer(props.id)}>Rendre</div>
             </div>}
         </div>
