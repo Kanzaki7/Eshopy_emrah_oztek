df7e3a0d867f0ee678ad5b9910de54edaab5920a
Emrah <emrahketzo@gmail.com>
2024-02-21 11:52:32 +0100

    ajout style

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -7,11 +7,11 @@ export default function Articles(props) {
                 <div className='divImage'>
                     <img src={process.env.PUBLIC_URL + props.img} alt="" />
                 </div>
-                <div className={(props.id === 0 && props.stock1 === 1) ? `divImageContent-orange${props.id}`: (props.id === 0 && props.stock1 === 0) ? `divImageContent-red${props.id}` : (props.id === 1 && props.stock2 === 1) ? `divImageContent-orange${props.id}`: (props.id === 1 && props.stock2 === 0) ? `divImageContent-red${props.id}` : (props.id === 2 && props.stock3 === 1) ? `divImageContent-orange${props.id}` : (props.id === 2 && props.stock3 === 0) && `divImageContent-red${props.id}` && "divImgContent" }>
+                <div>
                     <p className='boisson'>{props.nom}</p>
                     <p className='prix'>Prix : {props.prix}€</p>
                     <p className='stock'>Stock : {props.champi.stock} unités</p>
-                    {props.argent >= props.champi.prix && props.champi.stock > 0 ? <div className={`btnAcheter${props.id}`} onClick={()=>props.achatBis(props.id)}>Acheter</div> : null}
+                    {props.argent >= props.champi.prix && props.champi.stock > 0 ? <div className="btnAcheter" onClick={()=>props.achatBis(props.id)}>Acheter</div> : null}
                 </div>
             </div>
         </div>
